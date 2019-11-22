67980c435aeb86a24db331cfe54d7b65f67d496c
tomotaka68 <hanasaka68@gmail.com>
2019-11-22 18:21:45 +0900

    'message23'

---
+++ static/polls/js/quiz2.js
@@ -0,0 +1,389 @@
+var data4=[
+['オペレーティングシステム','コンピュータのオペレーション（操作・運用・運転）を司るシステムソフトウェアである。'],
+['共有メモリ','情報処理において複数のプログラムが同時並行的にアクセスするメモリ'],
+['データベース','検索や蓄積が容易にできるよう整理された情報の集まり。'],
+['プロセス間通信','コンピュータの動作において、複数プロセス（の複数スレッド）間でデータをやりとりする仕組み。通信プロセスは、同一コンピュータ内で帰結するローカル、ネットワーク接続された別のコンピュータと相互にリモート、などのほかに多様な観点で分類され、スレッド間の通信帯域幅とレイテンシや扱うデータの種類も多種多様である。'],
+['IPアドレス（アイピーアドレス）','「Internet Protocol Address」の略称,インターネット上でパソコンなどの通信機器を識別するための番号'],
+['アクセスポイント','事業者が提供するネットワークに接続するための接続点。多くの事業者が全国複数箇所にアクセスポイントを用意している。ユーザーは、専用線や電話回線などを使って近くのアクセスポイントに接続し、事業者の提供するネットワークサービスを利用する。'],
+['OSI(Open System Interconnection)','コンピュータの持つべき通信機能を階層構造に分割したモデルである。 国際標準化機構(ISO)によって策定された。 OSI基本参照モデル、OSIモデルなどとも呼ばれ、通信機能（通信プロトコル）を7つの階層に分けて定義している。'],['データリンク層','通信プロトコル（通信手順/通信規約）の機能や役割を階層構造で整理したモデルを構成する層の一つで、回線やネットワークで物理的に繋がれた二台の機器の間でデータの受け渡しを行うもの。'],
+['アプリケーション層','通信ネットワークにおいてホストが用いる共用のプロトコルとインターフェースメソッドを示す抽象化レイヤーである。この抽象概念はコンピュータネットワークの標準的なモデルであるインターネット・プロトコル・スイート( TCP/IP参照モデル)および開放型システム間相互接続モデル（OSI参照モデル）の両方で使われている。 '],
+['プレゼンテーション層','OSI参照モデルにおける七階層の内の第六層である。これはアプリケーション層からのサービス要求に応じ、またセッション層に対してサービス要求を行う。'],
+['セッション層','OSI参照モデルにおける七階層の内の第五層である。そこではプレゼンテーション層からのサービス要求に応じ、またトランスポート層に対してサービス要求を行う'],
+['トランスポート層','コンピュータと電気通信では、TCP/IP参照モデルにおけるの4階層の内の第3層の事である。上位のアプリケーション層からのサービス要求に応じ、また下位のインターネット層に対してサービス要求を行う。'],
+['ネットワーク層','OSI参照モデルにおける7階層の内の下から第3層の事である。TCP/IP参照モデルにおける4階層に対応付ける場合は、下から第2層のインターネット層に割り当てる。'],
+['物理層','OSI参照モデルにおける第一層で、伝送媒体（電気信号や光）上にてビット転送を行うための物理コネクションを確立・維持・解放する電気・機械・機能・手続き的な手段を定義する。 '],
+['Local Area Network（ローカル・エリア・ネットワーク）','広くても一施設内程度の規模で用いられるコンピュータネットワークのこと。その頭文字をつづったLAN（ラン）と書かれる場合も多い。一般家庭、企業のオフィスや研究所、工場等で広く使用されている。'],
+['インターネット・プロトコル・スイート','インターネットおよびインターネットに接続する大多数の商用ネットワークで利用できる通信規約（通信プロトコル）一式である。インターネット・プロトコル・スイートは、インターネットの黎明期に定義され、現在でも標準的に用いられている2つのプロトコル'],
+['ティム・バーナーズ＝リー','ロバート・カイリューとともにWorld Wide Web（WWW）を考案し、ハイパーテキストシステムを実装・開発した人物である。またURL、HTTP、HTML の最初の設計は彼によるものである。 '],
+['World Wide Web','インターネット上で提供されているハイパーテキストシステムである。'],
+['モデム（modem）','デジタル通信の送受信装置である。デジタル信号を伝送路の特性に合わせたアナログ信号にデジタル変調して送信するとともに、伝送路からのアナログ信号をデジタル信号に復調して受信するデータ回線終端装置の機能部分であり、通信方式は、ITU-Tにより標準化されている。'],
+['イーサネット','コンピューターネットワークの規格の1つ。世界中のオフィスや家庭で一般的に使用されている有線のLAN (Local Area Network) で最も使用されている技術規格で、OSI参照モデルの下位2つの層である物理層とデータリンク層に関して規定している。'],
+['10GBASE-T','UTPによる10ギガビット・イーサネット《10GbE》）規格'],
+['DHCP','UDP/IPネットワークで使用されるネットワーク管理プロトコルであり、コンピュータがネットワークに接続する際に必要な設定情報を自動的に割り当てるために使用する。'],
+['Domain Name System（ドメイン・ネーム・システム、DNS）','インターネットを使った階層的な分散型データベースシステムである。1983年にInformation Sciences Institute (ISI) のポール・モカペトリスとジョン・ポステルにより開発された。現在では主にインターネット上のホスト名や電子メールに使われるドメイン名と、IPアドレスとの対応づけ（正引き、逆引き）を管理するために使用されている。 '],
+['FTP','コンピュータネットワーク上のクライアントとサーバの間でファイル転送を行うための通信プロトコルの一つである。 '],
+['HTTP','HTMLなどのコンテンツの送受信に用いられる通信プロトコルである。主としてWorld Wide Webにおいて、WebブラウザとWebサーバとの間での転送に用いられる。日本標準仕様書ではハイパテキスト転送プロトコルとも呼ばれる。'],
+['Internet Message Access Protocol（インターネット メッセージ アクセス プロトコル、IMAP(アイマップ)','メールサーバ上の電子メールにアクセスし操作するためのプロトコル。クライアントとサーバがTCPを用いて通信する場合、通常サーバー側はIMAP4ではポート番号143番、IMAP over SSL(IMAPS)では993番を利用する。 '],
+['Internet Relay Chat（インターネット・リレー・チャット、略称 : IRC）','サーバを介してクライアントとクライアントが会話をする枠組みの名称である。インスタントメッセンジャーのプロトコルの一つに分類される。また、これに基づいて実装されるソフトをIRCクライアントと呼び、しばし略してクライアントもIRCと呼ばれる事がある。文章のみをやり取りして会話を行い、DCCなどを利用することでファイル転送も対応する。'],
+['Network Time Protocol（ネットワーク・タイム・プロトコル、略称NTP','ネットワークに接続される機器において、機器が持つ時計を正しい時刻へ同期するための通信プロトコルである。 '],
+['Simple Network Time Protocol（シンプルネットワークタイムプロトコル、SNTP','NTPパケットを利用した、簡単な時計補正プロトコルである。'],
+['Post Office Protocol（ポスト オフィス プロトコル、POP','電子メールで使われるプロトコル（通信規約）のひとつ。ユーザがメールサーバから自分のメールを取り出す時に使用する、メール受信用プロトコル。現在は、改良されたPOP3 (POP Version 3) が使用されている。 '],
+['Simple Mail Transfer Protocol（シンプル メール トランスファー プロトコル、SMTP','簡易メール転送プロトコルは、インターネットで電子メールを転送するプロトコルである。通常 ポート番号 25 を利用する。 転送先のサーバを特定するために、DNS の MXレコードが使われる。RFC 5321 で標準化されている。'],
+['Secure Shell（セキュアシェル、SSH）','暗号や認証の技術を利用して、安全にリモートコンピュータと通信するためのプロトコル。パスワードなどの認証部分を含むすべてのネットワーク上の通信が暗号化される。 '],
+['Telnet（テルネット Teletype network）','IPネットワークにおいて、遠隔地にあるサーバやルーター等を端末から操作する通信プロトコル、またはそのプロトコルを利用するソフトウェアである。RFC 854で規定されている。 '],
+['Extensible Messaging and Presence Protocol (XMPP)','オープンソースのインスタントメッセンジャーのプロトコルおよび、クライアント、サーバの総称である。'],
+['Transmission Control Protocol（トランスミッション コントロール プロトコル、TCP）','伝送制御プロトコルといわれ、インターネット・プロトコル・スイートの中核プロトコルのひとつ。'],
+['User Datagram Protocol（ユーザ データグラム プロトコル、UDP','主にインターネットで使用されるインターネット・プロトコル・スイートの中核プロトコルの一つ。'],
+['Internet Control Message Protocol（インターネット制御通知プロトコル、ICMP','通信処理で使われるプロトコルのひとつで、Internet Protocolのデータグラム処理における誤りの通知や通信に関する情報の通知などのために使用される。'],['Point-to-Point Protocol','2点間を接続してデータ通信を行うための通信プロトコルである。 ']
+];
+
+
+var n = 30;//問題数
+
+
+    ar = new Array(n);
+    for (y2=0; y2<ar.length; y2++){
+        ar[y2] = new Array(n);
+    }
+
+
+
+    function shuffle(array) {
+      var n = array.length, t, i;
+
+      while (n) {
+        i = Math.floor(Math.random() * n--);
+        t = array[n];
+        array[n] = array[i];
+        array[i] = t;
+      }
+
+      return array;
+    }
+
+
+
+    function randomSelect(array, num){
+        let newArray = [];
+        while(newArray.length < num && array.length > 0){
+            // 配列からランダムな要素を選ぶ
+            const rand = Math.floor(Math.random() * array.length);
+            // 選んだ要素を別の配列に登録する
+            newArray.push(array[rand]);
+            // もとの配列からは削除する
+            array.splice(rand, 2);
+        }
+        return newArray;
+    }
+
+
+    var ary = new Array();
+
+    for (y3=0; y3<ary.length; y3++){
+        ary[y3] = new Array();
+    }
+
+    var test_array = [ 1, 2, 3];
+
+    function select_quiz_gene(){
+
+        for(var st = 0; st <ar.length ; st++){
+
+
+            ary = data4;
+            rand=Math.floor(Math.random() * ary.length);
+            var random = ary[rand];
+            // もとの配列からは削除する
+            ary.splice(rand, 1);
+            //random2 =  new Array(ary.length);
+            random2 = ary;
+
+            const selected = randomSelect(random2.slice(), 2);
+
+            var randomNumber = Math.floor( Math.random() * (4 - 1) + 1 );
+
+
+
+            //ary = ary_origin;
+            // 配列arrayからランダムにnum個の要素を取り出す
+            //ary.push(ary[rand]);
+
+            t = shuffle(test_array);
+
+            ar[st][0] = random[0];
+            ar[st][t[0]] = random[1];
+            ar[st][t[1]] = selected[0][1];
+            ar[st][t[2]] = selected[1][1];
+            ar[st][4] = t[0];
+        }
+        return ar;
+    }
+    function getCol(matrix, col){
+       var column = [];
+       for(var i=0; i<matrix.length; i++){
+          column.push(matrix[i][col]);
+       }
+       return column;
+    }
+
+    var array = [
+        ['光ファイバー','光ファイバの説明'],
+        ['OSI参照モデル','OSI参照モデルの説明'],
+        ['インターネット層','インターネット層の説明'],
+        ['アプリケーション層','アプリケーション層の説明'],
+        ['ルーター','ルーターの説明']
+    ]; //..your 3x20 array
+    var a1 = getCol(array, 0); //Get first column
+
+    var qa = select_quiz_gene();
+
+    //count = 0; //問題番号
+    q_sel = 3; //選択肢の数
+
+    setReady();
+
+    //最初の問題
+    //quiz();
+
+    //初期設定
+    function setReady() {
+    	count = 0; //問題番号
+    	ansers = new Array(); //解答記録
+        ans_count = 0;
+    	//最初の問題
+    	quiz();
+    }
+
+    //問題表示
+    function quiz() {
+    	var s, n;
+    	//問題
+    	document.getElementById("text_q").innerHTML = (count + 1) + "問目：" + qa[count][0]+"の説明で正しいのを選べ";
+    	//選択肢
+    	s = "";
+    	for (n=1;n<=q_sel;n++) {
+    		s += "【<a href='javascript:anser(" + n + ")'>" + n + "：" + qa[count][n] + "</a>】"+"<br>"+"<br>";
+    	}
+    	document.getElementById("text_s").innerHTML = s;
+    }
+
+    //解答表示
+    function anser(num) {
+    	var s;
+    	s = (count + 1) + "問目：";
+    	//答え合わせ
+    	if (num == qa[count][q_sel + 1]) {
+    		//正解
+            ans_count++;
+    		s += "○" + qa[count][num]+"正解数"+ans_count;
+    	} else {
+    		s += "×" + qa[count][num]+"正解数"+ans_count;
+    	}
+    	document.getElementById("text_a").innerHTML = s;
+        //次の問題を表示
+        count++;
+        if (count < qa.length) {
+            quiz();
+        }
+        else {
+        //終了
+            document.getElementById("text_q").innerHTML = "";
+            document.getElementById("text_s").innerHTML = "";
+        }
+    }
+
+    //解答表示
+    function anser(num) {
+    	var s;
+    	s = (count + 1) + "問目：";
+    	//答え合わせ
+    	if (num == qa[count][q_sel + 1]) {
+    		//正解
+            ans_count++;
+    		s += "○" + qa[count][num]+"正解数"+ans_count;
+    		ansers[count] = "○";
+    	} else {
+    		s += "×" + qa[count][num]+"正解数"+ans_count;
+    		ansers[count] = "×";
+    	}
+    	document.getElementById("text_a").innerHTML = s;
+
+    	//次の問題を表示
+    	count++;
+    	if (count < qa.length) {
+    		quiz();
+    	} else {
+            //終了
+    		s = "<table border='2'><caption>成績発表</caption>";
+    		//1行目
+    		s += "<tr><th>問題</th>";
+    		for (n=0;n<qa.length;n++) {
+    			s += "<th>" + (n+1) + "</th>";
+    		}
+    		s += "</tr>";
+    		//2行目
+    		s += "<tr><th>成績</th>";
+    		for (n=0;n<qa.length;n++) {
+    			s += "<td>" + ansers[n] + "</td>";
+    		}
+    		s += "</tr>";
+    		s += "</table>";
+    		document.getElementById("text_q").innerHTML = "";
+            //次の選択肢
+    		s = "【<a href='javascript:history.back()'>前のページに戻る</a>】";
+    		s += "【<a href='javascript:setReady()'>同じ問題を最初から</a>】";
+    		s += "【<a href=''>次の問題に進む</a>】";
+    		document.getElementById("text_s").innerHTML = s;
+    	}
+    }
+    var qaa= data4;
+            var length = 20;
+            var ar_ox = new Array(1);
+            for(let y = 0; y < qaa.length; y++) {
+              ar_ox[y] = new Array(1).fill(0);
+            }
+            var ans_ox = new Array(1);
+            for(let y = 0; y < qaa.length; y++) {
+              ans_ox[y] = new Array(1).fill(0);
+            }
+
+    function dig_quest(){
+
+
+
+
+
+
+        for(var z = 0; z<ar_ox.length-1 ; z++){
+
+            var aryq = qaa;
+            randq=Math.floor(Math.random() * aryq.length);
+            var randomq = aryq[randq];
+
+            aryq.splice(randq, 1);
+
+            randomq2 = aryq;
+
+            var a_ox= randomq[0]
+
+            const selectedq = randomSelect(randomq2.slice(), 1);
+
+            rand2_1 =  Math.floor(Math.random()*2 + 1) ;
+            if(rand2_1==2){
+                var b_ox = selectedq[0][1];
+                ans_ox[z][0] = selectedq[0][0];
+                ans_ox[z][1] = selectedq[0][1]
+                var c_ox = a_ox + 'とは' + b_ox;
+                ar_ox[z][0] = c_ox;
+                ar_ox[z][1] = 2 ;
+            }
+            else if(rand2_1==1){
+                var d_ox =  selectedq[0][0] + 'とは' +selectedq[0][1];
+                ar_ox[z][0] = d_ox;
+                ar_ox[z][1] = 1;
+            }
+
+        }
+
+    }
+    dig_quest();
+    var qa_ox = [
+      ['「テトリス（ゲーム）」を開発したのは、日本人だ。', 2],
+      ['生きている間は、有名な人であっても広辞苑に載ることはない。 ', 1],
+      ['現在、2000円札は製造を停止している。', 1],
+      ['パトカーは、取り締まっている最中であれば、どこでも駐車違反になることはない。', 2],
+      ['昆虫の中には、口で呼吸する昆虫もいる。 ', 2],
+      ['一般的に体の水分は、男性より女性のほうが多く含まれている。', 2],
+      ['たとえ１ｃｍの高さであっても、地震によって起こるのは全て「津波」である。', 1],
+      ['1円玉の直径は、ちょうど１ｃｍになっている。', 2],
+      ['塩はカロリー０である。 ', 1],
+      ['「小中学校は、PTAを作らなければならない」と法律で定められている。', 2]
+    ];
+
+    var count_ox = 0;
+    var correctNum_ox = 0;
+
+    window.onload = function() {
+      // 最初の問題を表示
+      var question_ox = document.getElementById('question_ox');
+      question_ox.innerHTML = (count_ox + 1) + '問目：' + ar_ox[count_ox][0]+ '〇か×か？';
+    };
+
+    // クリック時の答え判定
+    function hantei(btnNo) {
+          if (ar_ox[count_ox][1] == btnNo) {
+            correctNum_ox++;
+          }
+
+          if (count_ox == ar_ox.length-1) {
+            alert('あなたの正解数は' + correctNum_ox + '問です！');
+          }
+
+          // 次の問題表示
+          count_ox++;
+          var question_ox = document.getElementById('question_ox');
+          question_ox.innerHTML = (count_ox + 1) + '問目：' + ar_ox[count_ox][0]+ '〇か×か？';
+          if(btnNo==1){
+              if(ar_ox[count_ox-1][1]==2){
+                  s = '2不正解2!' + ar_ox[count_ox-1][0] + 'は不正解.' +'<br><br>'+ '正解は'+ans_ox[count_ox-1][0]+'とは'+ ans_ox[count_ox-1][1];
+              }
+              else if(ar_ox[count_ox-1][1]==1){
+                  s ='1正解1!' + ar_ox[count_ox-1][0] + 'は正解';
+              }
+          }
+          if(btnNo==2){
+            if(ar_ox[count_ox-1][1]==2){
+                s = '4正解4!' + ar_ox[count_ox-1][0] + 'は不正解.' +'<br><br>'+ '正解は'+ans_ox[count_ox-1][0]+'とは'+ ans_ox[count_ox-1][1];
+            }
+            else if(ar_ox[count_ox-1][1]==1){
+                s ='3不正解3!' + ar_ox[count_ox-1][0] + 'は正解';
+            }
+            }
+          document.getElementById("text_ans").innerHTML = s;
+    }
+
+
+    var quizzes = data4;
+    var quiz2;
+
+    init();
+    function init(){
+        var ra = Math.floor(Math.random() * quizzes.length);
+        quiz2 = quizzes[ra];
+
+        //問題用のフォームに表示する
+
+        document.getElementById("questionana").innerHTML = quiz2[1];
+
+    }
+
+    function doAnswer(){
+        //回答用のフォームに入力した値を取得
+        var answerForm = document.querySelector("#answerana");
+        var answer = answerForm.value;
+
+        //回答用フォームで入力した内容を削除する
+        answerForm.value = "";
+
+        //入力した内容が正しいか調べる
+        if (answer == quiz2[0]) {
+            //入力した内容が正解の時
+            right();
+        } else {
+            //入力した内容が不正解の時
+            wrong();
+        }
+    }
+
+    //正解の時に実行する関数
+    function right(){
+        alert("正解です");
+
+        init();
+    }
+
+    //不正解の時に実行する関数
+    function wrong(){
+        alert("不正解です");
+
+    }
